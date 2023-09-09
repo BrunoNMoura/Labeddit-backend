3e3a5826bf9757ee294ff1f08e13c1ed4f0215a8
BrunoNMoura <bnmalemao@gmail.com>
2023-09-09 18:51:41 -0300

    :construction::bricks: dotenv criation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello World!");
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.listen(Number(process.env.PORT|| 3003), () => {
+    console.log(`Server running on port ${process.env.PORT}`)
+})
