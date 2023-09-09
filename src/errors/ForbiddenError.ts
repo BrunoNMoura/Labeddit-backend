2271e47044e47e6cc91746a06104528fd5cd61f9
BrunoNMoura <bnmalemao@gmail.com>
2023-09-09 19:32:27 -0300

    :construction::bricks: folder Errors

---
+++ src/errors/ForbiddenError.ts
@@ -0,0 +1,9 @@
+import { BaseError } from "./BaseError";
+
+export class ForbiddenError extends BaseError {
+    constructor(
+        message: string = "Valid token but not enough permissions"
+    ) {
+        super(403, message)
+    }
+}
---
+++ src/errors/NotFoundError.ts
@@ -0,0 +1,10 @@
+import { BaseError } from "./BaseError";
+
+export class NotFoundError extends BaseError {
+    constructor(
+        message: string = "resource not found"
+    ) {
+        super(404,message)
+    }
+    
+}
