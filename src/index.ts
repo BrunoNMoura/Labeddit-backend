1d86867450a4dd4eaad0f6a89a25907cda46a9a5
BrunoNMoura <bnmalemao@gmail.com>
2023-09-12 20:19:19 -0300

    :construction::bricks: post Router

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from 'express'
 import cors from 'cors'
 import dotenv from 'dotenv'
+import { userRouter } from './router/userRouter'
+import { postRouter } from './router/postRouter'
 
 dotenv.config()
 
@@ -12,3 +14,6 @@ app.use(express.json())
 app.listen(Number(process.env.PORT|| 3003), () => {
     console.log(`Server running on port ${process.env.PORT}`)
 })
+
+app.use("/users", userRouter)
+app.use("/posts", postRouter)
